78a4468b24feedc06faaf4176b501d4c32aa1dc9
Sandip <roysandip33290@gmail.com>
2024-05-21 13:12:45 +0530

    creating the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
-  return <div>Hello World</div>;
+  return(<div>
+    <section>
+      <Navbar/>
+    </section>
+    <section>Parallex</section>
+    <section>Services</section>
+    <section>Parallex</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+  </div>
+  )
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./navbar.scss"
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="wrapper">
+            <span>Hello</span>
+            <div className="social">
+                <a href="#"><img src="/facebook.png" alt=""/></a>
+                <a href="#"><img src="/instagram.png" alt=""/></a>
+                <a href="#"><img src="/github.png" alt=""/></a>
+                <a href="#"><img src="/youtube.png" alt=""/></a>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
