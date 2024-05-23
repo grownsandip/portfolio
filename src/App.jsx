14c5d1077a01f57de372aef1f1cb6be9519214f2
Sandip <roysandip33290@gmail.com>
2024-05-23 22:01:11 +0530

    Added the portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import "./app.scss"
+import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Home/Home";
 import Parallex from "./components/parallex/Parallex";
 import Services from "./components/Services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
 const App = () => {
-  return<div>
-    <section id="Homepage">
-    <Navbar/>
-    <Home/>
-    </section>
-    <section id="Services"><Parallex type="services"/></section>
-    <section><Services/></section>
-    <section id="Portfolio"><Parallex type="portfolio"/></section>
-    <section id="About">Parallex</section>
-    <section id="Contacts" >Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
-    <section>Contact</section>
-  </div>
-  
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Home />
+      </section>
+      <section id="Services">
+        <Parallex type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallex type="portfolio" />
+      </section>
+      <section>
+        <Portfolio/>
+      </section>
+      <section id="Contacts"></section>
+    </div>
+  );
 };
 
 export default App;
